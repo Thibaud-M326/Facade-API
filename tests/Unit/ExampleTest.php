4ebd93778b8c649ddcd6a99627235fcad484d30b
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-10-23 13:36:57 +0200

    before rebing/graphQL

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+
 class ExampleTest extends TestCase
 {
     /**
@@ -12,5 +13,45 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    // /**
+    //  * A test for user found by ID
+    //  */
+    // public function testQueriesUser(): void
+    // {
+    //     $response = $this->graphQL('
+    //         mutation ($id: ID!) {
+    //             createPost(title: $id) {
+    //                 id
+    //                 email
+    //                 first_name
+    //             }
+    //         }
+    //     ', [
+    //         'ID' => 1
+    //     ])->assertJson([
+    //     "data" => [
+    //             "user" => [
+    //                 "email" => "thibaudmaitre@gmail.com", 
+    //                 "first_name" => "Howell", 
+    //                 "last_name" => "Ankunding" 
+    //             ] 
+    //         ] 
+    //     ]);
+    // }
+
+
+    public function testQueriesPosts(): void
+    {
+        $response = $this->graphQL(/** @lang GraphQL */ '
+        {
+            users(first: 10, page: 10) {
+                data {
+                    id
+                }
+            }
+        }
+        ');
     }
 }
