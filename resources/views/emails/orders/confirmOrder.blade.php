9d880b6581ae3fdd78c6fe09b96d9c9cbae83d8c
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-11-13 20:28:53 +0100

    nothing addd just line added to screenshot

--- resources/views/emails/orders/confirmOrder.blade.php
+++ resources/views/emails/orders/confirmOrder.blade.php
@@ -1,8 +1,19 @@
-<p>
-    this is my mail
-    {{ $mailData[0] }}
-</p>
-<p>
-    ''
-    {{ $mailData[1] }}
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>confirmOrderMail</title>
+</head>
+<body>
+    <p>
+        this is my mail
+        {{ $mailData[0] }}
+    </p>
+    <p>
+        ''
+        {{ $mailData[1] }}
+    </p>
+</body>
+</html>
